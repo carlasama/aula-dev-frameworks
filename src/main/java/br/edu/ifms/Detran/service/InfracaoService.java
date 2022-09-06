92141ce5b6a1273c9bda931814190d43961f29af
Carla Samaniego <carla.samaniego@estudante.ifms.edu.br>
2022-09-06 09:00:56 -0400

    alterações services

--- src/main/java/br/edu/ifms/Detran/service/InfracaoService.java
+++ src/main/java/br/edu/ifms/Detran/service/InfracaoService.java
@@ -1,5 +1,13 @@
 package br.edu.ifms.Detran.service;
 
+import br.edu.ifms.Detran.dto.CarroDto;
+import br.edu.ifms.Detran.dto.InfracaoDto;
+import br.edu.ifms.Detran.model.Carro;
+import br.edu.ifms.Detran.model.Infracao;
+
 public class InfracaoService {
 
+	public Infracao fromDto(InfracaoDto objDto) {
+		return new Infracao(objDto.getId(), objDto.getDescricao(), objDto.getPontos(), objDto.getValor());
+	}
 }
--- src/main/java/br/edu/ifms/Detran/service/MultaService.java
+++ src/main/java/br/edu/ifms/Detran/service/MultaService.java
@@ -3,6 +3,9 @@ package br.edu.ifms.Detran.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.edu.ifms.Detran.dto.InfracaoDto;
+import br.edu.ifms.Detran.dto.MultaDto;
+import br.edu.ifms.Detran.model.Infracao;
 import br.edu.ifms.Detran.model.Multa;
 import br.edu.ifms.Detran.repository.MultaRepository;
 
@@ -15,5 +18,9 @@ public class MultaService {
 	public Multa inserir(Multa multa) {
 		multa.setId(null);
 		return repo.save(multa);
+	}
+	
+	public Multa fromDto(MultaDto objDto) {
+		return new Multa(objDto.getId(), objDto.getCidade(), objDto.getAno(), null, null);
 	}
 }
